339758cef6c8bb13cb2470368455497f7602c6c5
Anirban Choubey <anirbandreamsalot@gmail.com>
2018-02-09 11:30:10 +0530

    bubblesort propper

--- src/main/java/exp_algo/BubbleSort.java
+++ src/main/java/exp_algo/BubbleSort.java
@@ -1,9 +1,13 @@
 package exp_algo;
 
+/**
+ * @author anirban
+ *
+ */
 public class BubbleSort {
 
 	public static void main(String[] args) {
-		int[] unsortedArray = {2,3,4,9,33,111,44,12};
+		int[] unsortedArray = {2,3,4,9,33,111,666,44,12};
 		for(int i:unsortedArray)
 		System.out.println("unsorted array--"+i);
 		int [] sortedArray = doBubbleSort(unsortedArray);
@@ -11,14 +15,20 @@ public class BubbleSort {
 			System.out.println("sorted array---"+j);
 	}
 
+	/**
+	 * @param unsortedArray
+	 * @return sorted array
+	 * this sorting algo works on the principal of swapping 
+	 * 
+	 */
 	public static int[] doBubbleSort(int [] unsortedArray){
 		int[] SortedArray = null;
 		for(int i = 0 ;i<unsortedArray.length-1;i++){
-			for (int j =i+1;j<unsortedArray.length;j++)
-				if(unsortedArray[i]> unsortedArray[j]){
-					int bigNo = unsortedArray[i];
-					unsortedArray[i] = unsortedArray[j];
-					unsortedArray[j] = bigNo;
+			for (int j =i+1;j<unsortedArray.length-i-1;j++)
+				if(unsortedArray[j]> unsortedArray[j+1]){
+					int bigNo = unsortedArray[j];
+					unsortedArray[j] = unsortedArray[j+1];
+					unsortedArray[j+1] = bigNo;
 				}
 		}
 		SortedArray= unsortedArray;
